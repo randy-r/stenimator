837670fbb8ab6e44a5fe6f6a16bc6ab5552239ee
randy-r <31799713+randy-r@users.noreply.github.com>
2022-10-24 21:50:32 +0300

    Example app init.

---
+++ example/src/components/app-root/app-root.tsx
@@ -0,0 +1,16 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'app-root',
+  styleUrl: 'app-root.css',
+  shadow: true,
+})
+export class AppRoot {
+  render() {
+    return (
+      <div>
+        App Root
+      </div>
+    );
+  }
+}
---
+++ example/stencil.config.ts
@@ -0,0 +1,21 @@
+import { Config } from '@stencil/core';
+
+// https://stenciljs.com/docs/config
+
+export const config: Config = {
+  globalStyle: 'src/global/app.css',
+  globalScript: 'src/global/app.ts',
+  taskQueue: 'async',
+  tsconfig: 'tsconfig.json',
+  minifyJs: true,
+  minifyCss: true,
+  outputTargets: [
+    {
+      type: 'www',
+      // comment the following line to disable service workers in production
+      serviceWorker: null,
+      baseUrl: 'https://myapp.local',
+      prerenderConfig: './prerender.config.ts',
+    },
+  ],
+};
