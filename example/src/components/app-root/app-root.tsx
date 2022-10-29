62b2972735970f75e423f18b19b932bbeba03b93
randy-r <31799713+randy-r@users.noreply.github.com>
2022-10-29 19:08:13 +0300

    manual color scheme attempt

--- example/src/components/app-root/app-root.tsx
+++ example/src/components/app-root/app-root.tsx
@@ -13,13 +13,19 @@ export class AppRoot {
         <header class="header">
           <h4>Stenimator</h4>
           <span class="external-links">
+            <fieldset class="scheme-choice">
+              <input type="radio" id="dark-scheme" name="scheme" value="dark" />
+              <label htmlFor="dark-scheme">dark</label>
+              <input type="radio" id="light-scheme" name="scheme" value="light" />
+              <label htmlFor="light-scheme">light</label>
+            </fieldset>
             <a href="https://github.com/randy-r/stenimator" target="_blank">
               <Github />
             </a>
             <a href="https://www.npmjs.com/package/stenimator" target="_blank">
               <Npm />
             </a>
-            </span>
+          </span>
         </header>
         <section class="content">
           <router-standard />
