d506fa8597a2698c4de97d478c5fbd1701f8bb0c
randy-r <31799713+randy-r@users.noreply.github.com>
2022-10-29 16:49:50 +0300

    --brand2

--- example/src/components/app-root/app-root.tsx
+++ example/src/components/app-root/app-root.tsx
@@ -1,4 +1,5 @@
 import { Component, h, Host } from '@stencil/core';
+import { Github, Npm } from '../Icons/Icons';
 
 @Component({
   tag: 'app-root',
@@ -9,13 +10,23 @@ export class AppRoot {
   render() {
     return (
       <Host>
-        <header class="header"><h4>Stenimator</h4></header>
+        <header class="header">
+          <h4>Stenimator</h4>
+          <span class="external-links">
+            <a href="https://github.com/randy-r/stenimator" target="_blank">
+              <Github />
+            </a>
+            <a href="https://www.npmjs.com/package/stenimator" target="_blank">
+              <Npm />
+            </a>
+            </span>
+        </header>
         <section class="content">
-            <router-standard />
-            <router-ordered />
-            <router-individual />
-            <data-fetching />
-            <data-fetching-fast />
+          <router-standard />
+          <router-ordered />
+          <router-individual />
+          <data-fetching />
+          <data-fetching-fast />
         </section>
       </Host>
     );
